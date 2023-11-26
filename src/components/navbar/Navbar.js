4fa62364324c98b49b64f65c38f93866b6f0d8cf
gyulimjessKang <gyulim0606@gmail.com>
2023-11-26 14:57:32 +0900

    responsive navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -27,22 +27,68 @@ function Navbar() {
           </a>
           <div className="menu-tab">
             <div className="menu-items">
-              <Link to="/" className="nav-item">
+              <Link to="/" className="nav-item d-none d-md-block">
                 Home
               </Link>
-              <Link to="/announcement" className="nav-item">
+              <Link to="/announcement" className="nav-item d-none d-md-block">
                 Announcements
               </Link>
-              <a href="mailto:eaglecouncil@sas.edu.sg" className="nav-item">
+              <Link to="/announcement" className="nav-item d-md-none">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-megaphone" viewBox="0 0 16 16">
+  <path d="M13 2.5a1.5 1.5 0 0 1 3 0v11a1.5 1.5 0 0 1-3 0v-.214c-2.162-1.241-4.49-1.843-6.912-2.083l.405 2.712A1 1 0 0 1 5.51 15.1h-.548a1 1 0 0 1-.916-.599l-1.85-3.49a68.14 68.14 0 0 0-.202-.003A2.014 2.014 0 0 1 0 9V7a2.02 2.02 0 0 1 1.992-2.013 74.663 74.663 0 0 0 2.483-.075c3.043-.154 6.148-.849 8.525-2.199zm1 0v11a.5.5 0 0 0 1 0v-11a.5.5 0 0 0-1 0m-1 1.35c-2.344 1.205-5.209 1.842-8 2.033v4.233c.18.01.359.022.537.036 2.568.189 5.093.744 7.463 1.993V3.85zm-9 6.215v-4.13a95.09 95.09 0 0 1-1.992.052A1.02 1.02 0 0 0 1 7v2c0 .55.448 1.002 1.006 1.009A60.49 60.49 0 0 1 4 10.065m-.657.975 1.609 3.037.01.024h.548l-.002-.014-.443-2.966a68.019 68.019 0 0 0-1.722-.082z"/>
+</svg>
+              </Link>
+              <Link to="/feedback" className="nav-item d-none d-md-block">
+                Comments
+              </Link>
+              <Link to="/feedback" className="nav-item d-md-none">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-left-heart" viewBox="0 0 16 16">
+  <path d="M14 1a1 1 0 0 1 1 1v8a1 1 0 0 1-1 1H4.414A2 2 0 0 0 3 11.586l-2 2V2a1 1 0 0 1 1-1zM2 0a2 2 0 0 0-2 2v12.793a.5.5 0 0 0 .854.353l2.853-2.853A1 1 0 0 1 4.414 12H14a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+  <path d="M8 3.993c1.664-1.711 5.825 1.283 0 5.132-5.825-3.85-1.664-6.843 0-5.132Z"/>
+</svg>
+              </Link>
+              <a href="mailto:eaglecouncil@sas.edu.sg" className="nav-item d-none d-md-block">
                 Contact Us
               </a>
+              <Link to="mailto:eaglecouncil@sas.edu.sg" className="nav-item d-md-none">
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+  <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/>
+</svg>
+              </Link>
             </div>
           </div>
 
-          <div className="user-controls">
+
+
+
+
+
+
+
+          <div className="user-controls d-md-none">
+            <div className="">
+              <Link to="/mypage"  style={{ textDecoration: "none", color: "inherit" }}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+  <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+</svg>
+              </Link>
+            </div>
+            <div className="" onClick={signOut}>
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0z"/>
+  <path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+</svg>
+            </div>
+          </div>
+
+
+
+
+          
+          <div className="user-controls d-none d-md-flex">
             <div className="nav-link-box">
               <Link
-                to="/mypage"
+                to="/mypage" 
                 style={{ textDecoration: "none", color: "inherit" }}
               >
                 My Page
@@ -52,6 +98,12 @@ function Navbar() {
               Sign Out
             </div>
           </div>
+          
+
+
+
+
+
         </div>
       </nav>
     );
